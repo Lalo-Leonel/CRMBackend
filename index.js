ad718df5c35de1bee25a318d7209732df5b1f5ea
Leo <lionel.lalo@hotmail.com>
2021-06-09 12:52:02 -0500

    Inicial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const { ApolloServer, gpl } = require('apollo-server');
+
+// servidor
+const server = new ApolloServer();
+
+// arrancar el servidor
+server.listen().then( ({url}) => {
+    console.log('Servidor listo en la URL ${url}')
+})
